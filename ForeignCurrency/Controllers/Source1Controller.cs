0e5ce50033d0a8f827e29fa929d93577e81867a1
ajiybanesij <ajiybanesij@hotmail.com>
2019-08-16 23:25:31 +0300

    Emtia List Added

--- ForeignCurrency/Controllers/Source1Controller.cs
+++ ForeignCurrency/Controllers/Source1Controller.cs
@@ -171,5 +171,54 @@ namespace ForeignCurrency.Controllers
 
             return Ok(currencyList);
         }
+
+        [Route("source1/emtiaList")]
+        [HttpGet]
+        public async Task<IHttpActionResult> EmtiaList()
+        {
+            Uri URL = new Uri("https://www.doviz.com/emtialar");
+            List<Source1Model> currencyList = new List<Source1Model>();
+            string html = client.DownloadString(URL);
+
+            HtmlDocument document = new HtmlDocument();
+            document.LoadHtml(html);
+            try
+            {
+                var tableRow = document.DocumentNode.Descendants("tr");
+                int count = 0;
+
+                foreach (var node in tableRow)
+                {
+                    var array = node.InnerText.Replace(" ", "").Trim().Split('\n');
+                    if (count < 1)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+
+                        Source1Model model = new Source1Model();
+                        model.Name = _scripts.NameControl(array[0]);    // Currency Name
+                        model.Buyin = array[2];                         // Currency Buyin
+                        //model.Sales = array[4];                         // Currency Sales
+                        model.Change = array[4];                        // Currency Change
+                       // model.ChangeUpDown = null;                      //COMING
+                       // model.UpdateTime = array[11];                   // Currency Update Time
+
+                        count++;
+                        currencyList.Add(model);
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                return Ok("null");
+            }
+
+
+            return Ok(currencyList);
+        }
     }
 }
